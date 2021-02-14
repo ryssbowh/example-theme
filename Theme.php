661c653f00641cb0c6eb79874abfdacd105f3641
Boris Blondin <boris@inspire.scot>
2021-02-14 07:19:44 +0000

    added regions

--- Theme.php
+++ Theme.php
@@ -19,4 +19,45 @@ class Theme extends ThemePlugin
 	];
 
 	protected $selectable = false;
+
+	public function getRegions(): array
+	{
+		return [
+			new Region([
+				'handle' => 'header-left',
+				'name' => \Craft::t('themes', 'Header Left'),
+				'width' => '49%',
+			]),
+			new Region([
+				'handle' => 'header-right',
+				'name' => \Craft::t('themes', 'Header Right'),
+				'width' => '49%',
+			]),
+			new Region([
+				'handle' => 'before-content',
+				'name' => \Craft::t('themes', 'Before Content'),
+				'width' => '100%',
+			]),
+			new Region([
+				'handle' => 'content',
+				'name' => \Craft::t('themes', 'Content'),
+				'width' => '100%',
+			]),
+			new Region([
+				'handle' => 'after-content',
+				'name' => \Craft::t('themes', 'After Content'),
+				'width' => '100%',
+			]),
+			new Region([
+				'handle' => 'footer-left',
+				'name' => \Craft::t('themes', 'Footer Left'),
+				'width' => '49%',
+			]),
+			new Region([
+				'handle' => 'footer-right',
+				'name' => \Craft::t('themes', 'Footer Right'),
+				'width' => '49%',
+			])
+		];
+	}
 }
